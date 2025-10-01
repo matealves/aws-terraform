de001421da0dbfb3d8d62f25527a337458a89457
Mateus Alves <contatomateusalves@hotmail.com>
2025-10-01 20:21:09 -0300

    infra: create aws s3 bucket

---
+++ 01-primeiro-script/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = "1.13.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.14.1"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-2"
+  profile = "mateus-cli"
+}
+
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "terraform-test-bucket-1122"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+  }
+}
