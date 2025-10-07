9a7620816afd3ae16f5d9c8f32fb254a91ecdccd
Mateus Alves <contatomateusalves@hotmail.com>
2025-10-06 21:00:22 -0300

    feat: locals

--- interpolacao-locals-outputs/s3.tf
+++ interpolacao-locals-outputs/s3.tf
@@ -1,12 +1,13 @@
 resource "aws_s3_bucket" "test-bucket-2" {
   bucket = "${random_pet.bucket.id}-${var.environment}"
-
-  tags = local.common_tags
+  tags   = local.common_tags
 }
 
 resource "aws_s3_bucket_object" "this" {
-  bucket = aws_s3_bucket.test-bucket-2.bucket
-  key    = "config/${local.ip_filepath}"
-  source = "ips.json"
-  etag   = filemd5(local.ip_filepath)
+  bucket       = aws_s3_bucket.test-bucket-2.bucket
+  key          = "config/${local.ip_filepath}"
+  source       = "ips.json"
+  content_type = local.content_type
+
+  etag = filemd5(local.ip_filepath)
 }
